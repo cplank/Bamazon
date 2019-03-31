0f24086e48950a99979e3506fa6a8d2bfd80cb7a
cplank <cplankshepard@gmail.com>
2019-03-31 13:21:27 -0700

    initial commit

---
+++ bamazon.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: " ",
+    database: "bamazon_db"
+});
